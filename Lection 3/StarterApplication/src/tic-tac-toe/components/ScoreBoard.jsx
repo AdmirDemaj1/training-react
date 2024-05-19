1d6f5a08afac1dbf071bfee44d57b6a7e25a488a
Admir Demaj <a.demaj.ext@teamsystem.com>
2024-05-19 11:52:01 +0200

    apply styling for score board

--- Lection 3/StarterApplication/src/tic-tac-toe/components/ScoreBoard.jsx
+++ Lection 3/StarterApplication/src/tic-tac-toe/components/ScoreBoard.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 
-export const ScoreBoard = ({scores, xPlaying}) => {
-    const {XScore, OScore} = scores;
-    return (
-       <div className="scoreboard">
-        <span className={`score x-score ${!xPlaying && "inactive"}`}> X - {XScore}</span>
-        <span className={`score o-score ${xPlaying && "inactive"}`}>O - {OScore}</span>
-       </div>
-    )
-}
+import "./ScoreBoard.css";
+
+export const ScoreBoard = ({ scores, xPlaying }) => {
+  const { XScore, OScore } = scores;
+  return (
+    <div className="scoreboard">
+      <span className={`score x-score ${!xPlaying && "inactive"}`}>
+        {" "}
+        X - {XScore}
+      </span>
+      <span className={`score o-score ${xPlaying && "inactive"}`}>
+        O - {OScore}
+      </span>
+    </div>
+  );
+};
