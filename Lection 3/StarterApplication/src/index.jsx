d9877f1bba320c147d10f143d176fc5418ee32a3
Admir Demaj <a.demaj.ext@teamsystem.com>
2024-05-24 22:26:03 +0200

    fix styling for book library

--- Lection 3/StarterApplication/src/index.jsx
+++ Lection 3/StarterApplication/src/index.jsx
@@ -2,11 +2,11 @@ import ReactDOM from "react-dom/client";
 
 // import "./index.css";
 // import MainPage from "./mainPage/MainPage.jsx";
-import TicTacToe from "./tic-tac-toe/TicTacToe.jsx";
-import MyComponent from "./useRef/MyComponent.jsx";
-import ProductTableWithAutocomplete from "./useRef/ProductList.jsx";
-import TrackStateChangesComponent from "./useEffect/TrackStateChanges.jsx";
-import EventListenerComponent from "./useEffect/EventListenerComponent.jsx";
+// import TicTacToe from "./tic-tac-toe/TicTacToe.jsx";
+// import MyComponent from "./useRef/MyComponent.jsx";
+// import ProductTableWithAutocomplete from "./useRef/ProductList.jsx";
+// import TrackStateChangesComponent from "./useEffect/TrackStateChanges.jsx";
+// import EventListenerComponent from "./useEffect/EventListenerComponent.jsx";
 import BookLibrary from "./useEffect/BookLibrary.jsx";
 
 const entryPoint = document.getElementById("root");
