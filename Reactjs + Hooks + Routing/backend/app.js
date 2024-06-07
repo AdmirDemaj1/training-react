3d600409b78cb94d89271dc34fb10b253b20e40f
Admir Demaj <a.demaj.ext@teamsystem.com>
2024-06-08 01:36:16 +0200

    fix structure add backend

---
+++ Reactjs + Hooks + Routing/backend/app.js
@@ -0,0 +1,28 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+
+const booksRoutes = require("./routes/books");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+console.log("Starting application...");
+// Your app code
+console.log("Application started successfully");
+
+app.use("/books", booksRoutes);
+
+app.use((error, req, res, next) => {
+  const status = error.status || 500;
+  const message = error.message || "Something went wrong.";
+  res.status(status).json({ message: message });
+});
+
+app.listen(8080);
